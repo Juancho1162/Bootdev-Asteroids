98057be77ab0a7d5c8ce3485fff7a4ef62532f30
Juancho <juancho.prego.gundin@gmail.com>
2024-11-14 22:01:09 +0100

    ..

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+def main():
+    
+    print("Starting asteroids!")
+
+
+
+
+
+
+
+# The last lines ensures that main is only called by running this file.
+# So it cannot be imported as a module.
+if __name__ == "__main__":
+    main()
